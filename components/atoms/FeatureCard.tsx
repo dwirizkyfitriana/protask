9cafbf51c054a219da389914eaf2ba7e0b8b2386
Dwi Fitriana <dwifitriana@jumpa.id>
2023-10-23 12:03:08 +0700

    fix(about): fix feature card text color

--- components/atoms/FeatureCard.tsx
+++ components/atoms/FeatureCard.tsx
@@ -6,7 +6,7 @@ const FeatureCard = ({ title, body }: { title: string; body: string }) => {
     <div className='bg-white rounded-xl p-4 grid grid-cols-[24px_1fr] gap-4 items-start shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
       <Icon className='text-primary' path={mdiCheckCircle} size={1} />
 
-      <div className='space-y-2'>
+      <div className='space-y-2 text-black'>
         <h1 className='font-semibold text-xl'>{title}</h1>
         <p>{body}</p>
       </div>
